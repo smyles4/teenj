456015e87ef20795e3adf40e83df998078d922af
smyles4 <smyles4@cps.edu>
2025-03-13 14:38:29 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("work");
+
+let boy = document.querySelector(".boy");
+colsole.log(boy)
+let girl = document.querySelector(".girl");
+colsole.log(girl)
+ let quiz = document.querySelector(".quiz");
+ colsole.log(quiz)
+
+ quiz.onclick=function(){
+    console.log("work button")
+    let answerBoy=boy.value
+    console.log("answerBoy")
+    let answerGirl=girl.value
+    console.log("answerGirl")
+    if(answerBoy===john){
+        console.log("hey john")
+    }
+    if(answerGirl===kate){
+        console.log("hey kate")
+    }
+
+
+};
+
+
