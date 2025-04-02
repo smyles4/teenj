c3d3fbd2fcb7d1c4f4017bd83a9eb3060833ff74
smyles4 <smyles4@cps.edu>
2025-04-02 14:40:10 +0000

    123

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-console.log("work");
 
-let boy = document.querySelector(".boy");
-colsole.log(boy)
-let girl = document.querySelector(".girl");
-colsole.log(girl)
- let quiz = document.querySelector(".quiz");
- colsole.log(quiz)
+console.log("ok");
 
- quiz.onclick=function(){
-    console.log("work button")
-    let answerBoy=boy.value
-    console.log("answerBoy")
-    let answerGirl=girl.value
-    console.log("answerGirl")
-    if(answerBoy===john){
-        console.log("hey john")
-    }
-    if(answerGirl===kate){
-        console.log("hey kate")
-    }
+ let haircolor = ("brown");
+ let favoritecolor = ("pink");
+let button = document.querySelector("button");
 
+//  button.addEventListener("click", function() {
+//      let haircolor = document.querySelector(".haircolor").value;
+//      console.log(haircolor);
+//      let favoritecolor = document.querySelector(".favoritecolor").value;
+//         console.log(favoritecolor);
+//              let message = document.querySelector(".message");
 
-};
+//  {
+// console.log("great")
+//  message.innerHTML = "sususs"
+//  } if(haircolor==="brown" && favoritecolor!=="pink"){
+//  console.log("bad")
+//  message.innerHTML= "your robin"
+//  }else if(haircolor!=="brown" && favoritecolor==="pink"){
+//  console.log("womp")
+//  message.innerHTML="your supermans cousin twice removed"
+//  }else if(haircolor!=="brown" && favoritecolor!=="pink"){
+//  console.log("omg")
+//  message.innerHTML="your a fish"
+// }
 
 
