15959e136e1f7f75d19134f8f8c83f9c61b9da48
smyles4 <smyles4@cps.edu>
2025-04-16 14:39:40 +0000

    dg

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 
 console.log("ok");
 
-let haircolor =document.querySelector("haircolor");
-let favoritecolor =document.querySelector("favoritecolor");
+let haircolor =document.querySelector(".haircolor");
+let favoritecolor =document.querySelector(".favoritecolor");
 let button = document.querySelector("button");
-
+let message = document.querySelector(".message");
 button.onclick = function() {
     console.log("button check");
-};
- console.log(haircolor);
+let hair = haircolor.value;
+ console.log("haircolor");
+ let color = favoritecolor.value;
+  console.log("favoritecolor");
+
+if(hair==="brown" && color ==="pink"){
+    console.log("cookie")
+    message.innerHTML = "you like cookies"
+  } else if (hair==="brown" && color ==="pink"){
+    console.log("cookie")
+    message.innerHTML = "you like cookies"
+  } else if (hair==="black" && color ==="blue"){
+    console.log("cool beans")
+    message.innerHTML = " your a cool bean"
+  }else if (hair==="blonde" && color ==="yellow"){
+    console.log("sunflower")
+    message.innerHTML = "sunflower hero"
+  }
   
-        console.log(favoritecolor);
-             let message = document.querySelector(".message");
-
- 
-console.log("great");
- message.innerHTML = "sususs"
-  if(haircolor==="brown" && favoritecolor!=="pink");
- console.log("bad")
-
-//else if(haircolor!=="brown" && favoritecolor==="pink")
- //console.log("womp")
-// message.innerHTML="your supermans cousin twice removed"
-//}else if(haircolor!=="brown" && favoritecolor!=="pink")
-//console.log("omg")
- //message.innerHTML="your a fish"
+};
